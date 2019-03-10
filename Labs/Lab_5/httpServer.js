6097610477149b9019a6f1f777e4c4c2789d2653
joningi98 <joningi17201@gmail.com>
2019-03-10 22:50:38 +0000

    lab5

--- Labs/Lab_5/httpServer.js
+++ Labs/Lab_5/httpServer.js
@@ -1,18 +1,22 @@
 const http = require('http');
-const URL = 'http://127.0.0.1:3000/divide?a=[value]&b=[value]';
-const hostname = '127.0.0.1';
+const url = require('url');
 const port = 3000;
 const math = require('./math');
 
 var s = http.createServer();
 s.on('request', function(request, response) {
     response.writeHead(200);
-    console.log(request.method);
-    if (request.method === 'GET' && request.url == '/divide?a=[value]&b=[value]'){
-        let my_url = new URL(URL);
-        let a = my_url.searchParams('/divide?a=[value]&b=[value]').value;
-        console.log(a);
-        console.log(math.doDivition(4,2))
+    let res_url = url.parse(request.url, true);
+    if (request.method === 'GET' && res_url.pathname === '/divide'){
+        response.statusCode = 200;
+        var url_data = res_url.query;
+        var a = url_data.a;
+        var b = url_data.b;
+        console.log(math.stringifyDivision(a,b))
+    }
+    else{
+        response.statusCode = 405;
+        response.send('This operation is not supported')
     }
    // console.log(request.headers);
     // console.log(request.url);
